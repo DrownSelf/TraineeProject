65f90a88e3fa1960ec72a920ab3420a01f093571
DrownSelf <strigelskiy.petr@gmail.com>
2022-01-05 13:07:15 +0300

    first commit

---
+++ TraineeProject/Program.cs
@@ -0,0 +1,29 @@
+﻿static void PrintMatrix(int[,] outputed)
+{
+    for (int i = 0; i < outputed.GetLength(0); i++)
+    {
+        for (int j = 0; j < outputed.GetLength(1); j++)
+        {
+            Console.Write(string.Format("{0} ", outputed[i, j]));
+        }
+        Console.Write(Environment.NewLine + Environment.NewLine);
+    }
+}
+
+int[,] numArr;
+Console.WriteLine("Enter size of matrix: ");
+
+Console.WriteLine("m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("n: ");
+int n = int.Parse(Console.ReadLine());
+
+numArr = new int[n, m];
+
+TraineeProject.Algotighm.RandomFill(numArr);
+PrintMatrix(numArr);
+TraineeProject.Algotighm.Find(numArr);
+TraineeProject.Algotighm.Find(numArr);
+
+string output = TraineeProject.Translator.LatinConverter(numArr);
+Console.WriteLine(output);
---
+++ TraineeProject/Translator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraineeProject
+{
+    internal static class Translator
+    {
+        public static string LatinConverter(int[,] translationed)
+        {
+            string translated = "";
+            for (int i = 0; i < translationed.GetLength(0); i++)
+            {
+                for (int j = 0; j < translationed.GetLength(1); j++)
+                    translated += char.ConvertFromUtf32(translationed[i, j] + 97);
+                //int a = 5;
+                //object b = a;
+                //char c = (char)(int)b;
+                //Console.WriteLine(c); Доп.метод
+                translated += '\n';
+            }
+            return translated;
+        }
+    }
+}
